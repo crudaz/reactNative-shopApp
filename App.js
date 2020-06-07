8e343e3639adf26c8907c27688ee2c6f854fbcde
crudaz <romerocruz@gmail.com>
2020-06-06 21:48:01 -0500

    feat: cardReducer, orderReducer

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import * as React from 'react';
-import { createStore, combineReducers } from 'redux';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import productsReducer from './store/reducers/products';
-import ShopNavigator from './navigation/ShopNavigator';
+import * as React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+// import { AppLoading } from "expo";
+// import * as Font from "expo-font";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import productsReducer from "./store/reducers/products";
+import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/orders";
+
+import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default function App() {
   return (
     <Provider store={store}>
-        <ShopNavigator />
+      <ShopNavigator />
     </Provider>
   );
 }
